b365c292ce8eee0df80d6463aab9d955156b7ef1
IOsetting <iosetting@outlook.com>
2022-10-04 23:41:18 +0800

    fix: avoid "_close is not implemented" warnings

--- Libraries/Debug/debug.c
+++ Libraries/Debug/debug.c
@@ -1,3 +1,6 @@
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
 #include "debug.h"
 #include "ddl.h"
 #include "uart.h"
@@ -69,4 +72,44 @@ __attribute__((weak)) int _write(int file, char *ptr, int len)
         __io_putchar(*ptr++);
     }
     return len;
+}
+
+__attribute__((weak)) int _isatty(int fd)
+{
+    if (fd >= STDIN_FILENO && fd <= STDERR_FILENO)
+        return 1;
+
+    errno = EBADF;
+    return 0;
+}
+
+__attribute__((weak)) int _close(int fd)
+{
+    if (fd >= STDIN_FILENO && fd <= STDERR_FILENO)
+        return 0;
+
+    errno = EBADF;
+    return -1;
+}
+
+__attribute__((weak)) int _lseek(int fd, int ptr, int dir)
+{
+    (void)fd;
+    (void)ptr;
+    (void)dir;
+
+    errno = EBADF;
+    return -1;
+}
+
+__attribute__((weak)) int _fstat(int fd, struct stat *st)
+{
+    if (fd >= STDIN_FILENO && fd <= STDERR_FILENO)
+    {
+        st->st_mode = S_IFCHR;
+        return 0;
+    }
+
+    errno = EBADF;
+    return 0;
 }
