00bb653526924ee1465d1bc4af090cad882541ab
IOsetting <iosetting@outlook.com>
2023-04-12 21:07:19 +0800

    fix: set p01, p02 as default uart1 port

--- Libraries/BSP/bsp_printf.h
+++ Libraries/BSP/bsp_printf.h
@@ -10,7 +10,7 @@
 // UART port for printf output
 #define DEBUG_UARTx         DEBUG_UART1
 // UART port pins for printf output
-#define DEBUG_UART_PORT     DEBUG_UART_P35P36
+#define DEBUG_UART_PORT     DEBUG_UART_P01P02
 
 void Bsp_PrinfInit(uint32_t baud);
 
