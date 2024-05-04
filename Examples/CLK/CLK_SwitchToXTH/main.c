0f8f9549464d91adc6a79e7cfbde64a119e0a9ad
IOsetting <iosetting@outlook.com>
2024-05-05 00:08:04 +0800

    fix: add uart conflict notes, add delay to avoid locked swd

--- Examples/CLK/CLK_SwitchToXTH/main.c
+++ Examples/CLK/CLK_SwitchToXTH/main.c
@@ -2,9 +2,16 @@
  * Switch system clock to external high speed oscillator
  * 
  * Wiring:
- *   P01 & P02  -> 32MHz Quartz OSC
+ *   P01 & P02  -> 32MHz XTAL OSC
  *   P34        -> + LED1 -  -> GND
  *   P35(TX)    -> USB2TTL RX
+ * 
+ * XTAL OSC port(P01 & P02) conflict with UART port, need to change UART port to P35 & P36
+ * 
+ * Edit bsp_printf.h, change `DEBUG_UART_PORT` to DEBUG_UART_P35P36
+ * ```
+ *   #define DEBUG_UART_PORT     DEBUG_UART_P35P36
+ * ```
 */
 #include "gpio.h"
 #include "bsp_printf.h"
@@ -12,6 +19,8 @@
 int main(void) 
 {
     uint32_t u32Val;
+
+    delay1ms(2000);
     /**
      * Switch to external high speed oscillator
      * 
