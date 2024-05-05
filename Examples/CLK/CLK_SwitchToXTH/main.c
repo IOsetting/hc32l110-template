ea96001b40dba27f91f6788779a200bdd3c611ff
IOsetting <iosetting@outlook.com>
2024-05-06 01:48:19 +0800

    refactor: update external clock source example

--- Examples/CLK/CLK_SwitchToXTH/main.c
+++ Examples/CLK/CLK_SwitchToXTH/main.c
@@ -4,11 +4,11 @@
  * Wiring:
  *   P01 & P02  -> 32MHz XTAL OSC
  *   P34        -> + LED1 -  -> GND
- *   P35(TX)    -> USB2TTL RX
  * 
- * XTAL OSC port(P01 & P02) conflict with UART port, need to change UART port to P35 & P36
+ * XTAL OSC port(P01 & P02) conflicts with UART port, if you are going to use printf, 
+ * please change the UART port to P35 & P36 by editing bsp_printf.h, 
  * 
- * Edit bsp_printf.h, change `DEBUG_UART_PORT` to DEBUG_UART_P35P36
+ * change `DEBUG_UART_PORT` to DEBUG_UART_P35P36
  * ```
  *   #define DEBUG_UART_PORT     DEBUG_UART_P35P36
  * ```
@@ -18,34 +18,23 @@
 
 int main(void) 
 {
-    uint32_t u32Val;
-
-    delay1ms(2000);
+    delay1ms(1000);
     /**
      * Switch to external high speed oscillator
      * 
      * Edit "CLK_XTH_VAL" in clk.h to change the frequency
     */
     Clk_SwitchTo(ClkXTH);
-    u32Val = Clk_GetHClkFreq();
 
-    Bsp_PrinfInit(115200);
-
-    /**
-     * Set P34 as output
-    */
-    Gpio_InitIOExt(3, 4, 
-                    GpioDirOut, // Output
-                    FALSE,      // No pull up
-                    FALSE,      // No pull down
-                    FALSE,      // No open drain
-                    FALSE);     // High driver capability
+    // Set P34 as output
+    Gpio_InitIOExt(3, 4, GpioDirOut, FALSE, FALSE, FALSE, FALSE);
+    // Turn it off
+    GPIO_SetPinOutLow(3, 4);
 
     // Toggle LED blink
     while (1)
     {
-        printf("Sysclk %ld\r\n", u32Val);
         GPIO_TogglePinOut(3, 4);
-        delay1ms(1000);
+        delay1ms(500);
     }
 }
